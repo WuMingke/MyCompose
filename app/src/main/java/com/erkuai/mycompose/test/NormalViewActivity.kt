34fb22533e10cf13edbd00663c180b781af8d0c2
wumingke <766190553@qq.com>
2022-09-11 18:19:16 +0800

    Modifier 1.1

---
+++ app/src/main/java/com/erkuai/mycompose/test/NormalViewActivity.kt
@@ -0,0 +1,13 @@
+package com.erkuai.mycompose.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.erkuai.mycompose.R
+
+class NormalViewActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_normal_view)
+    }
+}
